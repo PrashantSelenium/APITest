63214f0a3e01b013eecc7f0cc7b355c3f207ff36
Pratik Barjatya <pratikb@transerv.local>
2016-05-26 11:20:49 +0530

    Commit Merged

--- libs/Driver.py
+++ libs/Driver.py
@@ -4,12 +4,9 @@ import sys, getopt
 import os
 import LoadEnvironment
 import ReadWriteExcel
-import ExecuteTestCase
 
-filename = "\TestCase Repository.xlsx"
+
 SHMART,username,password = LoadEnvironment.getEnvironment(sys.argv)
-testcases,testsheet = ReadWriteExcel.getTotalTestCases(filename)
-for testcase in range(2, testcases + 1):
-	testData = ReadWriteExcel.readFromExcelSheet(testcase,testsheet)
-	responseJson = ExecuteTestCase.runAPI(testData,SHMART,username,password)
-	print responseJson
+print SHMART
+print username
+print password
---
+++ libs/ExecuteTestCase.py
@@ -0,0 +1,28 @@
+'''
+@This is used to process the APIs
+@Author : Runjhun Singh
+'''
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import os, sys
+currentFilePath = os.path.abspath(__file__)	
+rootPath = "\\".join(currentFilePath.split("\\")[:-2])
+sys.path.append(rootPath)
+import ReadWriteExcel 
+
+# base_url = "http://stag.shmart.in"
+# username = "0ada3354cbf5ef4c1848778cb415dbda"
+# password = "df2b09cbaf274a10833900e0dcc75f7f"
+
+def runAPI(testData,SHMART,username,password):
+	base_url = SHMART 
+	if testData.method == "GET":
+		test_url = base_url + testData.api_url_append
+		response = requests.get(test_url, auth=HTTPBasicAuth(username, password))
+	elif testData.method == "POST":
+		test_url = base_url + testData.api_url_append
+		headers = {'content-type': 'application/json'}
+		response = requests.post(test_url, auth=HTTPBasicAuth(username, password),data=json.dumps(testData.request_body),headers=headers)
+		responseJson = response.json()
+	return responseJson
