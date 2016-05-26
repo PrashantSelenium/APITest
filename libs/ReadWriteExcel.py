fb5cfc0aae82cca19d47cd5e8fca4f8eb3aaa7ac
Runjhun Singh <runjhuns@transerv.local>
2016-05-26 21:15:06 +0530

    Changes made to fetch variable values from testData

--- libs/ReadWriteExcel.py
+++ libs/ReadWriteExcel.py
@@ -3,43 +3,70 @@
 @Author : Runjhun Singh
 '''
 import openpyxl
+import openpyxl.cell
+from openpyxl.utils import (_get_column_letter)
+
 import os, sys
 from openpyxl.styles import colors , Font, Color 
 import ExecuteTestCase
+import re
 currentFilePath = os.path.abspath(__file__)	
 rootPath = "\\".join(currentFilePath.split("\\")[:-2])
 sys.path.append(rootPath)
-from utils.TestData import TestData
+from utils.TestCaseValues import TestCaseValues
 
-# def startProcess(SHMART,username,password):
-# input_file = getExcelFilePath(filename)
-# 	readFromExcelSheet(filename,SHMART,username,password)	
-sheet_name = "TestSuite1"
-	
+
 def getExcelFilePath(filename):
-	print "--finding path of the excel file----"
+	# print "--finding path of the excel file----"
 	currentFilePath = os.path.abspath(__file__)	
 	rootPath = "\\".join(currentFilePath.split("\\")[:-2])
-	print rootPath
+	# print rootPath
 	input_file_path = rootPath+filename
 	return input_file_path
 
-def getTotalTestCases(filename):
-	input_file_path = getExcelFilePath(filename)
+def openExcelSheet(input_file_path,sheet_name):
 	wb = openpyxl.load_workbook(input_file_path)
 	sheet = wb.get_sheet_by_name(sheet_name)
+	return sheet
+
+def getTotalTestCases(filename):
+	input_file_path = getExcelFilePath(filename)
+	sheet = openExcelSheet(input_file_path,"TestSuite1")
 	row = sheet.max_row
 	return row , sheet
 
 def readFromExcelSheet(row,sheet):
-	# input_file_path = getExcelFilePath(filename)
-	# wb = openpyxl.load_workbook(input_file_path)
-	# sheet = wb.get_sheet_by_name(sheet_name)
-	# for row in range(2, sheet.max_row + 1):
 	if (sheet['A' + str(row)].value != ""):
-		testData = TestData(sheet,row)
-		print testData.keyword
-		return testData
+		testCaseValues = TestCaseValues(sheet,row)
+		print testCaseValues.keyword
+		# print testCaseValues.request_body
+		testCaseValues.request_body = createRequestBody(testCaseValues.request_body)	
+		# print testCaseValues.request_body
+		return testCaseValues
+
+def createRequestBody(request):
+	# print request
+	request = re.sub(r'#\w+', replaceData, request)
+	return request
+
+def replaceData(match):
+	match = match.group()
+	matching_value = getTestCaseValuesFromExcel(match)
+	return  matching_value
+
+def getTestCaseValuesFromExcel(variable):
+	# print variable
+	input_file_path = getExcelFilePath("\TestCase Repository.xlsx")
+	sheet = openExcelSheet(input_file_path,"TestData")
+	counter = 0
+	for column in range(1,20):
+	    column_letter = _get_column_letter(column)
+	    for row in range(1,sheet.max_row):
+	        if sheet[column_letter + str(row)].value == variable:
+		        cell = sheet[column_letter + str(row+1)].value
+
+		        return "\"" + str(cell) + "\""
+		
 
 def writeIntoExcelSheet():
 	a1 = sheet['K' + str(row)]
